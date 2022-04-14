16f2d2ac3baba8b4d7e30d27a0f6e7a7fba97574
thales <thalesdaniel2013@gmail.com>
2022-04-14 19:22:08 -0300

    feat: estruturado router dom

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
 );
