8e84de127ec02b921c99ba3d3b8a1766d80709c4
thales <thalesdaniel2013@gmail.com>
2022-04-14 19:41:40 -0300

    feat: estruturado rotas e criado paginas

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Login from './pages/login';
+import Costumer from './pages/costumer';
+import Register from './pages/register';
+import Details from './pages/details';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/costumer" element={<Costumer />} />
+      <Route path="/details" element={<Details />} />
     </Routes>
   </BrowserRouter>,
 );
---
+++ src/pages/costumer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Costumer() {
+  return (
+    <div>
+      Costumer
+    </div>
+  );
+}
+
+export default Costumer;
